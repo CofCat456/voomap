65cdcd37f4ab395730164236a4308dd9a2e69322
CofCat456 <a25690756@gmail.com>
2023-08-02 11:29:44 +0800

    fix: fix GoogleMap Components type intelliSense

--- packages/map/src/types/index.d.ts
+++ packages/map/src/types/index.d.ts
@@ -1,5 +1,6 @@
 import { MapConfig } from "@vue3-google-map/types";
+import { DefineComponent } from "vue";
 
 declare module "@cofcat/vue3-google-map" {
-  export const GoogleMap: MapConfig ;
+  export const GoogleMap: DefineComponent<MapConfig> ;
 }
