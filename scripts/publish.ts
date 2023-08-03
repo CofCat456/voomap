34dadf15d74d554a7e41d9f04271846b67872cf7
CofCat456 <a25690756@gmail.com>
2023-08-03 21:56:50 +0800

    fix(map): fix publish error

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,4 +1,4 @@
 import { execSync as exec } from 'node:child_process';
 
 exec('pnpm --filter @voomap/core publish:ci', { stdio: 'inherit' });
-exec('pnpm --filter @voomap publish:ci', { stdio: 'inherit' });
+exec('pnpm --filter @voomap/map publish:ci', { stdio: 'inherit' });
