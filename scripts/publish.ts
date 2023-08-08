d2b4727728353576524c506a1cf36265bb8be156
CofCat456 <a25690756@gmail.com>
2023-08-09 05:50:11 +0800

    fix: fix build error

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,4 +1,17 @@
 import { execSync as exec } from 'node:child_process';
 
-exec('pnpm --filter @voomap/core publish:ci', { stdio: 'inherit' });
-exec('pnpm --filter @voomap/map publish:ci', { stdio: 'inherit' });
+import path, { resolve } from 'node:path';
+import { copyFileSync } from 'fs-extra';
+
+exec('pnpm build', { stdio: 'inherit' });
+
+const command = 'pnpm -r publish --access public --no-git-checks';
+
+const readmePath = path.join(resolve(__dirname, '..'), 'README.md');
+const mapReadmePath = path.join(resolve(__dirname, '../packages/map/README.md'));
+const coreReadmePath = path.join(resolve(__dirname, '../packages/core/README.md'));
+
+copyFileSync(readmePath, mapReadmePath);
+copyFileSync(readmePath, coreReadmePath);
+
+exec(`${command}`, { stdio: 'inherit' });
