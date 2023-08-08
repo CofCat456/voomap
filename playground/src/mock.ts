3557c20b9557af82d58cb25e75490388367f89e9
CofCat456 <a25690756@gmail.com>
2023-08-08 19:01:41 +0800

    feat(playground): change the mock coordinate parameter

--- playground/src/mock.ts
+++ playground/src/mock.ts
@@ -1,6 +1,6 @@
 export function createRandomCoordinate() {
-  const randomLat = 24 + Math.random();
-  const randomLng = 121 + Math.random();
+  const randomLat = 22 + Math.random() * 3;
+  const randomLng = 120.5 + Math.random();
 
   const lat = Number.parseFloat(randomLat.toFixed(6));
   const lng = Number.parseFloat(randomLng.toFixed(6));
