b102e5f4ba0cd939c4d463e09e135d02bb9b7747
CofCat456 <a25690756@gmail.com>
2023-08-12 16:33:28 +0800

    feat(core): support search location

---
+++ packages/core/src/composables/useSearch.ts
@@ -0,0 +1,73 @@
+import type { Ref } from 'vue';
+import { markRaw, onMounted, reactive, ref, toValue } from 'vue';
+import { useMap } from './useMap';
+
+export function useSearch(apiKey: string) {
+  const isLoading = ref(false);
+  const cGoogle = ref<typeof google>();
+  const cApi = ref<typeof google.maps>();
+  const cGeocoder = ref<google.maps.Geocoder>();
+  const coordinates = reactive<google.maps.LatLngLiteral>({
+    lat: 0,
+    lng: 0,
+  });
+
+  function resetCoordinates() {
+    Object.assign(coordinates, {
+      lat: 0,
+      lng: 0,
+    });
+  }
+
+  const getCoordinates = async (address: string | Ref<string>) => {
+    isLoading.value = true;
+
+    if (!cGeocoder.value)
+      return;
+
+    if (!address && !toValue(address))
+      return;
+
+    const request: google.maps.GeocoderRequest = {
+      address: toValue(address),
+    };
+
+    // NOTE: 使用地理編碼服務將地址轉換為地理座標
+    await cGeocoder.value.geocode(request, (results, status) => {
+      if (status === 'OK' && results && results.length > 0) {
+        const { location } = results[0].geometry;
+
+        if (!location)
+          return;
+
+        const { lat, lng } = location;
+
+        if (!lat || !lng)
+          return;
+
+        // 設置座標
+        Object.assign(coordinates, { lat: lat(), lng: lng() });
+
+        isLoading.value = false;
+      }
+      else {
+        resetCoordinates();
+
+        isLoading.value = false;
+      }
+    });
+  };
+
+  onMounted(async () => {
+    const { loader } = useMap(apiKey);
+    cGoogle.value = markRaw(await loader.load());
+    cApi.value = markRaw(cGoogle.value.maps);
+    cGeocoder.value = markRaw(new cApi.value.Geocoder());
+  });
+
+  return {
+    isLoading,
+    coordinates,
+    getCoordinates,
+  };
+}
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,2 @@
 export * from './composables/useMap';
+export * from './composables/useSearch';
