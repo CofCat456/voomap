fff58d05d043d250ff16e4eaba62d8be23f0b9d1
CofCat456 <a25690756@gmail.com>
2023-09-01 18:28:04 +0800

    feat(map): add embed parameter

--- packages/map/src/components/Embed/src/index.vue
+++ packages/map/src/components/Embed/src/index.vue
@@ -1,22 +1,46 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 interface Props {
+  mapMode: 'place' | 'view' | 'directions' | 'streetview' | 'search'
   apiKey: string
+  width?: string
+  height?: string
+  style?: string
+  allowfullscreen?: boolean
+  address?: string
+  center?: string
+  zoom?: number
+  mapType?: 'roadmap' | 'satellite'
 }
 
-const props = withDefaults(defineProps<Props & {
-  width: string
-  height: string
-  style: string
-}>(), {
+const props = withDefaults(defineProps<Props>(), {
+  mapMode: 'place',
   width: '450',
   height: '250',
   style: 'border:0',
+  allowfullscreen: true,
+  address: 'Eiffel+Tower,Paris+France',
 });
 
+const baseUrl = ref('https://www.google.com/maps/embed/v1');
+
 const getUrl = computed(() => {
-  return `https://www.google.com/maps/embed/v1/place?key=${props.apiKey}&q=Eiffel+Tower,Paris+France`;
+  let url = `${baseUrl.value}/${props.mapMode}?key=${props.apiKey}`;
+
+  if (props.address)
+    url += `&q=${props.address}`;
+
+  if (props.center)
+    url += `&center=${props.center}`;
+
+  if (props.zoom)
+    url += `&zoom=${props.zoom}`;
+
+  if (props.mapType)
+    url += `&mapType=${props.mapType}`;
+
+  return url;
 });
 </script>
 
@@ -24,7 +48,7 @@ const getUrl = computed(() => {
   <iframe
     frameborder="0"
     referrerpolicy="no-referrer-when-downgrade"
-    allowfullscreen
+    :allowfullscreen="allowfullscreen"
     :src="getUrl"
     :width="width"
     :height="height"
