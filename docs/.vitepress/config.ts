7d4ed5ebae10b0f289d59adedb12104d145620e6
CofCat456 <a25690756@gmail.com>
2023-12-29 03:14:54 +0800

    docs(docs): fix header github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/CofCat456/voomap' },
     ],
 
     footer: {
